c577e9169d9f16ad3cc8741f4f40a7b15a8f083a
Joaco Salas <joaco.salas20@gmail.com>
2024-10-24 17:54:55 -0300

    mini update

--- src/routes/barRoutes.js
+++ src/routes/barRoutes.js
@@ -2,35 +2,15 @@ const express = require('express');
 const db = require('../config/db');
 const router = express.Router();
 
-// // Ruta para obtener productos de un bar específico
-// router.get('/:bar_id/products', async (req, res) => {
-//   const { bar_id } = req.params;
-
-//   try {
-//     const query = 'SELECT id, name, price, image_url FROM Product WHERE bar_id = $1';
-//     const { rows } = await db.query(query, [bar_id]);
-
-//     if (rows.length === 0) {
-//       return res.status(404).json({ message: 'No se encontraron productos para este bar' });
-//     }
-
-//     res.status(200).json(rows);
-//   } catch (error) {
-//     console.error('Error al obtener los productos:', error);
-//     res.status(500).json({ error: 'Error al obtener los productos del bar' });
-//   }
-// });
-
-
 
 // Ruta para obtener todos los bares
 router.get('/bars', async (req, res) => {
   try {
-    // Cambié 'id' a 'bar_id' para reflejar la estructura correcta de tu base de datos
+    //Cambié 'id' a 'bar_id' para reflejar la estructura correctamente
     const result = await db.query('SELECT bar_id AS id, business_name, address FROM "Bar"');
     res.status(200).json(result.rows);
   } catch (error) {
-    console.error('Error al obtener los bares:', error);  // Añadir para mayor visibilidad en los errores
+    console.error('Error al obtener los bares:', error);  //Añadir para mayor visibilidad en los errores
     res.status(500).json({ error: 'Error al obtener los bares' });
   }
 });
@@ -39,6 +19,7 @@ router.get('/bars', async (req, res) => {
 router.get('/bars/:bar_id/products', async (req, res) => {
   const { bar_id } = req.params; // Obtiene el bar_id de los parámetros de la ruta
   console.log('bar_id recibido:', bar_id); // Para verificar el valor de bar_id
+
   try {
     const result = await db.query('SELECT * FROM "Product" WHERE bar_id = $1', [bar_id]);
     console.log('Resultados obtenidos:', result.rows); // Muestra los resultados obtenidos
